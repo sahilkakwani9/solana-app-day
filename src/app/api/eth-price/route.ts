ea9be3aa6ace507fe312c865da3804f3effa2bc5
sahilkakwani9 <sahkakwani@gmail.com>
2024-11-05 23:20:41 +0530

    fea: fleek deploy

--- src/app/api/eth-price/route.ts
+++ src/app/api/eth-price/route.ts
@@ -1,3 +1,4 @@
+export const runtime = 'edge';
 import { NextResponse } from "next/server";
 
 const COINGECKO_KEY = process.env.COINGECKO_KEY;
--- src/app/api/vote/route.ts
+++ src/app/api/vote/route.ts
@@ -1,3 +1,4 @@
+export const runtime = 'edge';
 import { NextResponse } from "next/server";
 import { z } from "zod";
 import prisma from "../../../../prisma/client";
